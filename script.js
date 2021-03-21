87ace3116776ebcd9c28724ba69a6edc578e9504
Harry Gustave <harry23gust@gmail.com>
2021-03-21 17:00:02 -0500

    ,added fucntion ot sotre data on local sotrage, also added color scheneme to comapare time

--- script.js
+++ script.js
@@ -28,3 +28,54 @@ for (time = 9; time <= 17; time++) {
   };
   workDayPlanner.push(dataPlanner);
 }
+
+//current Day Header
+function currentDate() {
+    var cDate = moment().format("dddd, MMMM Do");
+    $("#currentDay").text(cDate);
+  }
+  //localStorage store data
+  function storePlannerData() {
+    localStorage.setItem("dayPlanner", JSON.stringify(workDayPlanner));
+  }
+  // localStorage Data display
+  function plannerDataDisplay() {
+    workDayPlanner.forEach(function (hour) {
+      $("#" + hour.id).val(hour.dataPlanner);
+    });
+  }
+  
+  //Load Data
+  function dataLoader() {
+    var dataLoad = JSON.parse(localStorage.getItem("dayPlanner"));
+    if (dataLoad) {
+      workDayPlanner = dataLoad;
+    }
+    storePlannerData();
+    plannerDataDisplay();
+  }
+
+  workDayPlanner.forEach(function (hour) {
+    var tRow = $("<form>");
+    tRow.addClass("row");
+    $(".container").append(tRow);
+  
+    var tField = $("<div>");
+    tField.addClass("col-md-2 hour");
+    tField.text(hour.dHour + hour.ampm);
+  
+    var hInput = $("<div>");
+    hInput.addClass("col-md-9 description p-0");
+  
+    var hData = $("<textarea>");
+    hData.attr("id", hour.id);
+  
+    //compare times - color codes
+    if (hour.time == moment().format("HH")) {
+      hData.addClass("present");
+    } else if (hour.time < moment().format("HH")) {
+      hData.addClass("past");
+    } else if (hour.time > moment().format("HH")) {
+      hData.addClass("future");
+    }
+    hInput.append(hData);
